7162386904b74ef22917d321b4338b53190fdbbb
John Wells <molekewl2017@gmail.com>
2022-03-17 02:43:10 -0700

    place mainWindow below system tray app icon. Added 23 to y incase user has hide menu

--- index.js
+++ index.js
@@ -20,10 +20,19 @@ app.on('ready', () => {
     process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
-  tray.on('click', () => {
+
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds;
+    // when a user resizes the window getBounds will updated height/width
+    console.log(mainWindow.getBounds())
+    const { width, height } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
-      mainWindow.hide();
+      mainWindow.hide(); 
     } else {
+      mainWindow.setBounds({
+        x: x - width / 2, y: 23, width, height,
+      })
       mainWindow.show();
     }
   });
